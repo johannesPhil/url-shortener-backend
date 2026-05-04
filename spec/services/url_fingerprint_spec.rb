f5c233d9fe74d8d535074000ae5fd1fcefa411f2
johannesPhil <jay3pee@gmail.com>
2026-05-04 16:25:44 +0100

    fix: use correct RSpec matcher

--- spec/services/url_fingerprint_spec.rb
+++ spec/services/url_fingerprint_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe UrlIdentifier do
   it "returns hex digest of the normalized URL" do
     fingerprint = described_class.call("https://example.com/path?a=1&b=2")
 
-    expect(fingerprint.length).to eql?(64)
+    expect(fingerprint.length).to eql(64)
     expect(fingerprint).to match(/\A[0-9a-f]{64}\z/)
   end
 end
