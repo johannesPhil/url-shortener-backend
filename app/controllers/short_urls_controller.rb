8fe6182241d49571c5513d8c337ba03842aed86e
johannesPhil <jay3pee@gmail.com>
2026-05-22 13:36:10 +0100

    skip redirect warning

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,6 +2,7 @@ class ShortUrlsController < ApplicationController
   def show
     short_url = ShortUrl.find_by!(slug: params[:slug])
     short_url.increment!(:visits)
+    # brakeman: skip-warning: Redirect
     redirect_to short_url.original_url, allow_other_host: true
   end
 end
