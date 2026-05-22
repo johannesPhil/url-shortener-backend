15ddb9c183319c08b978ed4ccebb3416908125bb
johannesPhil <jay3pee@gmail.com>
2026-05-22 14:11:48 +0100

    security(brakeman): ignore redirect warning

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -2,7 +2,7 @@ class ShortUrlsController < ApplicationController
   def show
     short_url = ShortUrl.find_by!(slug: params[:slug])
     short_url.increment!(:visits)
-    # brakeman: skip-warning: Redirect
+    # Intentional off-site redirect for URL shortener destinations.
     redirect_to short_url.original_url, allow_other_host: true
   end
 end
